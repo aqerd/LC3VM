fa92b6039bf6f506ffa93b25655a97ff6a45889e
Ruslan Suleymanov <87182605+aqerd@users.noreply.github.com>
2024-12-30 23:11:46 +0500

    New data

--- include/lc3.h
+++ include/lc3.h
@@ -2,9 +2,14 @@
 #define LC3
 
 #include <stdint.h>
+#include "trap.h"
+
+#define MEMORY_MAX (1 << 16)
+uint16_t memory[MEMORY_MAX];
 
 uint16_t reg[R_COUNT];
 
+/* Registers */
 enum {
     R_R0 = 0,
     R_R1,
@@ -19,12 +24,14 @@ enum {
     R_COUNT
 };
 
+/* Flags */
 enum {
     FL_POS = 1 << 0,
     FL_ZRO = 1 << 1,
     FL_NEG = 1 << 2,
 };
 
+/* Opcodes */
 enum {
     OP_BR = 0,  /* branch */
     OP_ADD,     /* add  */
@@ -40,8 +47,19 @@ enum {
     OP_STI,     /* store indirect */
     OP_STR,     /* store register */
     OP_RTI,     /* unused */
+    OP_RET,     /* return from procedure */
     OP_TRAP,    /* execute trap */
     OP_RES      /* reserved (unused) */
+};
+
+/* TRAP routines */
+enum {
+    TRAP_GETC = 0x20,  /* get character from keyboard, not echoed onto the terminal */
+    TRAP_OUT = 0x21,   /* output a character */
+    TRAP_PUTS = 0x22,  /* output a word string */
+    TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
+    TRAP_PUTSP = 0x24, /* output a byte string */
+    TRAP_HALT = 0x25   /* halt the program */
 };
 
 #endif
