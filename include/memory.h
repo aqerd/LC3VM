6980d8d92a3382ea14d45943ff6bf75847d80c93
Ruslan Suleymanov <87182605+aqerd@users.noreply.github.com>
2024-12-30 23:14:14 +0500

    Memory keywords and read/write functions

---
+++ include/memory.h
@@ -0,0 +1,14 @@
+#ifndef MEMORY
+#define MEMORY
+
+#include <stdint.h>
+
+enum {
+    MR_KBSR = 0xFE00, /* keyboard status */
+    MR_KBDR = 0xFE02  /* keyboard data */
+};
+
+void mem_write(uint16_t address, uint16_t val);
+uint16_t mem_read(uint16_t address);
+
+#endif
